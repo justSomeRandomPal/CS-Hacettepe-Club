928567d34932dc6304706d43f48257dfdb3b8a0f
Serkan Korkut <serkankorkut@users.noreply.github.com>
2016-11-09 01:31:26 +0400

    Add files via upload

--- hafta4/charpointer.c
+++ hafta4/charpointer.c
@@ -7,18 +7,34 @@ void aStrCpy(char ** destPtr, char * source);
 int main(int argc, char const *argv[]) {
 
 
- char * source="Serkan Korkut";
- char * dest = (char*)calloc(100,sizeof(char));
+ char * source = (char*)calloc(100,sizeof(char));
+
+ source[0]='a';
+
+ source[1]='b';
+
+ source[2]='c';
+
+ char * dest ;
 
 
  aStrCpy(&dest,source);
 
  printf("%s\n",dest );
 
+dest[2]='m';
+
+ printf("%s\n",dest );
+
+
+
+
+
 
   return 0;
 }
-
+/* If you do not allocate source and try to modify destination's characters by hand,
+ you'll get an error since source and dest will be constants and read only  */
 void aStrCpy(char ** destPtr, char * source)
 {
 
